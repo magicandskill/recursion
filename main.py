7840652011070ae6dec5d8f214a95a5d3c605667
Andy <andy@Andys-MacBook-Pro.local>
2024-01-02 13:08:24 -0500

    factorial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-nums = [1, 2, 3, 4, 5, 6, 7]
+data = [1, 2, 3, 4, 5, 6, 7]
 
 def sum(data):
   total = 0
@@ -6,13 +6,23 @@ def sum(data):
     total += n
   return total
 
-def re_add(current_sum, i, data):
+def re_add(sum, i, data):
   if(i >= len(data)):
-    return current_sum
+    return sum
   else:
-    new_sum = current_sum + data[i]
-    return re_add(new_sum, i+1, data)
+    return re_add(sum+data[i], i+1)
 
-total = re_add(0, 0, nums)
+def _factorial(total, i):
+  if(i == 0):
+    return 1
+  else:
+    new_total = _factorial(total, i-1)
+    return new_total * i
+
+def factorial(i):
+  return _factorial(0, i)
+
+total = re_add(0, 0, data)
 
 print(total)
+print(factorial(5))
